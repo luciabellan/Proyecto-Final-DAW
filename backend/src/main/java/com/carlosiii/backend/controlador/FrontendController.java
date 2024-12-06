943613cd8a2d7033d8887b17d07583e70a9c932f
Lucia Martinez Abellan <4273068@alu.murciaeduca.es>
2024-12-06 23:53:55 +0100

    frontend controller

---
+++ backend/src/main/java/com/carlosiii/backend/controlador/FrontendController.java
@@ -0,0 +1,14 @@
+package com.carlosiii.backend.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping(value = "/**/{path:[^\\.]*}")
+    public String redirect() {
+        // Redirige todas las rutas no manejadas al index.html de React
+        return "forward:/index.html";
+    }
+}
