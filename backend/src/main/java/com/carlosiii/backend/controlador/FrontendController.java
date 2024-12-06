d1d7f141fb7b53e017b65e2951abb469f19cf0f7
Lucia Martinez Abellan <4273068@alu.murciaeduca.es>
2024-12-07 00:06:35 +0100

    path frontend controller

--- backend/src/main/java/com/carlosiii/backend/controlador/FrontendController.java
+++ backend/src/main/java/com/carlosiii/backend/controlador/FrontendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FrontendController {
 
-    @GetMapping(value = "/**/{path:[^\\.]*}")
+    @GetMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         // Redirige todas las rutas no manejadas al index.html de React
         return "forward:/index.html";
