86fd67a81dc122ccfbb490eb85a62defffa95a31
Lucia Martinez Abellan <4273068@alu.murciaeduca.es>
2024-12-08 19:00:16 +0100

    cambios en index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './styles.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import CustomizationProvider from './context/CustomizationProvider';
+import React from "react"; // Biblioteca React para crear componentes
+import ReactDOM from "react-dom/client"; // Herramienta para renderizar en el DOM
+import "./index.css";
+import "./styles.css";
+import App from "./App"; // Componente principal de la aplicación
 
+import CustomizationProvider from "./context/CustomizationProvider"; // Contexto para la personalización
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CustomizationProvider>
+    <CustomizationProvider> 
       <App />
     </CustomizationProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
