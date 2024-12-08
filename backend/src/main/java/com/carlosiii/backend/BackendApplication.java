7ecceeef7e9ef5e3ff78c69880aac2edd86039a5
Lucia Martinez Abellan <4273068@alu.murciaeduca.es>
2024-12-08 13:44:11 +0100

    comentarios clase inicia app Spring Boot

--- backend/src/main/java/com/carlosiii/backend/BackendApplication.java
+++ backend/src/main/java/com/carlosiii/backend/BackendApplication.java
@@ -2,11 +2,12 @@ package com.carlosiii.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+// Clase principal que inicia la aplicación Spring Boot
+@SpringBootApplication // Anotación que combina @Configuration, @EnableAutoConfiguration y @ComponentScan
 public class BackendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {// Método main que inicia la aplicación
+		// Inicia el contenedor de Spring Boot y la aplicación
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
